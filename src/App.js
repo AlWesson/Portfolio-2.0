221d90905a66a66f3de9e3a96c3381ca412f6e6d
Al <awessonn@gmail.com>
2023-06-29 23:20:21 -0700

    init and delpoy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from 'react';
+import * as THREE from 'three';
+
+import { Canvas } from '@react-three/fiber';
+import { ContactShadows, Environment, Float, Html, OrbitControls } from '@react-three/drei'
+import { MathUtils } from 'three'
+import {  Project } from './components';
+import './style.css';
+
+
+const material = new THREE.MeshStandardMaterial(); 
+
+// creating a variety of shapes within an array
+const geometries = [
+  {geometry: new THREE.TetrahedronGeometry(2)},
+  {geometry: new THREE.OctahedronGeometry(2)},
+  {geometry: new THREE.IcosahedronGeometry(2)},
+  {geometry: new THREE.DodecahedronGeometry(2, 0)},
+]
+
+// fucntion to randomize 50 different shapes to be rendered onto the screen 
+function GEOS () {
+  const amount = 45;
+  const randomizeGeo = useMemo(() => Array.from({length: amount}, () => geometries[Math.floor(Math.random()*geometries.length)]), []);
+  return randomizeGeo.map((prop) => {
+    return (
+      <Float>
+        <mesh 
+          scale={MathUtils.randFloat(0.25, 0.5)}
+          position={[MathUtils.randFloat(-10, 10), MathUtils.randFloat(-10, 10), MathUtils.randFloat(-10, 10)]}
+          geometry={prop.geometry}
+          material={material}
+        />
+      </Float>
+    )
+  })
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Canvas camera={{position: [1, 0, 22.5]}}>
+      
+      <Float floatIntensity={10} rotationIntensity={1}>
+        <Html receiveShadow castShadow transform>
+        <iframe title="embed" 
+            width={1920} 
+            height={1080} 
+            src="https://alwesson.github.io/port-simple/" 
+            />
+        </Html>
+      </Float>
+      <GEOS />
+      <Environment background preset='lobby' blur={0.8} />
+      <OrbitControls />
+      <ContactShadows position={[0, -9, 0]} opacity={0.7} scale={40} blur={1} />
+    </Canvas>
     </div>
   );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,29 @@
+import { Typography } from "@material-ui/core";
+
+export function Project() {
+    
+    return (
+        <div>
+            
+            <iframe title="embed" 
+            width={1024} 
+            height={576} 
+            src="https://alwesson.github.io/port-simple/" 
+            />
+
+           
+        </div>
+    );
+  
+}
+
+export default Project;
+
+/* <iframe
+            data="https://github.com/AlWesson"
+            width='1024'
+            height='576'
+            type="text/html">
+
+            </iframe> */
+         
